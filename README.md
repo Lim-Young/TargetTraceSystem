c72fe67ad92e560fe4eb4c3419a38f5ed4827627
Lim Young <lim.young@foxmail.com>
2025-06-02 18:06:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TargetTraceSystem
+A universal target tracking system that enables users to centrally manage various target tracking needs, particularly well-suited for AI systems.
---
+++ Source/TargetTraceSystem/Private/TargetTraceSystem.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TargetTraceSystem.h"
+
+#define LOCTEXT_NAMESPACE "FTargetTraceSystemModule"
+
+void FTargetTraceSystemModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FTargetTraceSystemModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FTargetTraceSystemModule, TargetTraceSystem)
