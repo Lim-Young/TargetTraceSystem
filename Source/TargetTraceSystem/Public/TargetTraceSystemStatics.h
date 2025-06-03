cca798e67d8dd4bcf77a11d379866b1fe605ff9a
Lim Young <lim.young@foxmail.com>
2025-06-03 12:47:30 +0800

    Add auxiliary function for predicting target location

---
+++ Source/TargetTraceSystem/Public/TargetTraceSystemStatics.h
@@ -0,0 +1,20 @@
+﻿// Copyright Lim Young.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TargetTraceSystemStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TARGETTRACESYSTEM_API UTargetTraceSystemStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION()
+	static FVector GetTargetPredictedLocation(const FVector& SourceLocation, const AActor* Actor, const float LeadTargetProjectileVelocity,
+	                                    const bool bLeadTargetIgnoreZVelocity = false);
+};
